b26ad711bc5c17aa756203d908280d9df367bc2c
HYE GOON LEE <60787008+gooniii@users.noreply.github.com>
2020-08-28 17:38:26 +0900

    resume

---
+++ js/portfolios.js
@@ -0,0 +1,42 @@
+let bg = document.getElementById("sky");
+let moon = document.getElementById("moon");
+let cloud = document.getElementById("cloud");
+let man = document.getElementById("man");
+let text = document.getElementById("text");
+
+    window.addEventListener('scroll', function(){
+
+    var value = window.scrollY;
+        sky.style.top = value * 0.5 + 'px';
+        moon.style.left = -value * 0.5 + 'px';
+        cloud.style.top = -value * 0.004 + 'px';
+        man.style.top = value * 0.15 + 'px';
+        text.style.top = value * 1 + 'px';
+    })
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item =document.querySelectorAll('.header .nav-bar .nav-list ul li a')
+const header = document.querySelector('.header.container');
+
+    hamburger.addEventListener('click',()=> {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    })
+
+    document.addEventListener('scroll',()=> {
+        var scroll_position = window.scrollY;
+
+        if(scroll_position > 250) {
+            header.style.backgroundColor = "#29323c";
+        } else {
+            header.style.backgroundColor = 'transparent';
+        }
+    })
+
+    menu_item.forEach(item=>{
+        item.addEventListener('click', ()=>{
+            hamburger.classList.toggle('active');
+            mobile_menu.classList.toggle('active');
+        })
+    })
